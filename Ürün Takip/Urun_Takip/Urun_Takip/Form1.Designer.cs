a556a2537d4b5d98cb24edf72e2d0d3d493968e0
Pinar Aliogullari <aliogullarpnar@gmail.com>
2023-10-03 15:09:51 +0300

    dosyalar güncellendi

---
+++ Ürün Takip/Urun_Takip/Urun_Takip/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Urun_Takip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.TxtKategoriAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_Listele = new System.Windows.Forms.Button();
+            this.Btn_Kaydet = new System.Windows.Forms.Button();
+            this.Btn_Sil = new System.Windows.Forms.Button();
+            this.Btn_Güncelle = new System.Windows.Forms.Button();
+            this.Btn_Ara = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 133);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(577, 301);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textID);
+            this.groupBox1.Controls.Add(this.TxtKategoriAd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(36, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(577, 77);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(400, 25);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(138, 26);
+            this.textID.TabIndex = 3;
+            // 
+            // TxtKategoriAd
+            // 
+            this.TxtKategoriAd.Location = new System.Drawing.Point(124, 22);
+            this.TxtKategoriAd.Name = "TxtKategoriAd";
+            this.TxtKategoriAd.Size = new System.Drawing.Size(135, 26);
+            this.TxtKategoriAd.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(282, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Urun ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kategori Adı:";
+            // 
+            // Btn_Listele
+            // 
+            this.Btn_Listele.Location = new System.Drawing.Point(636, 133);
+            this.Btn_Listele.Name = "Btn_Listele";
+            this.Btn_Listele.Size = new System.Drawing.Size(98, 40);
+            this.Btn_Listele.TabIndex = 2;
+            this.Btn_Listele.Text = "Listele";
+            this.Btn_Listele.UseVisualStyleBackColor = true;
+            this.Btn_Listele.Click += new System.EventHandler(this.Btn_Listele_Click);
+            // 
+            // Btn_Kaydet
+            // 
+            this.Btn_Kaydet.Location = new System.Drawing.Point(636, 200);
+            this.Btn_Kaydet.Name = "Btn_Kaydet";
+            this.Btn_Kaydet.Size = new System.Drawing.Size(98, 40);
+            this.Btn_Kaydet.TabIndex = 7;
+            this.Btn_Kaydet.Text = "Kaydet";
+            this.Btn_Kaydet.UseVisualStyleBackColor = true;
+            this.Btn_Kaydet.Click += new System.EventHandler(this.Btn_Kaydet_Click);
+            // 
+            // Btn_Sil
+            // 
+            this.Btn_Sil.Location = new System.Drawing.Point(636, 259);
+            this.Btn_Sil.Name = "Btn_Sil";
+            this.Btn_Sil.Size = new System.Drawing.Size(98, 40);
+            this.Btn_Sil.TabIndex = 8;
+            this.Btn_Sil.Text = "Sil";
+            this.Btn_Sil.UseVisualStyleBackColor = true;
+            this.Btn_Sil.Click += new System.EventHandler(this.Btn_Sil_Click);
+            // 
+            // Btn_Güncelle
+            // 
+            this.Btn_Güncelle.Location = new System.Drawing.Point(636, 327);
+            this.Btn_Güncelle.Name = "Btn_Güncelle";
+            this.Btn_Güncelle.Size = new System.Drawing.Size(98, 40);
+            this.Btn_Güncelle.TabIndex = 9;
+            this.Btn_Güncelle.Text = "Güncelle";
+            this.Btn_Güncelle.UseVisualStyleBackColor = true;
+            this.Btn_Güncelle.Click += new System.EventHandler(this.Btn_Güncelle_Click);
+            // 
+            // Btn_Ara
+            // 
+            this.Btn_Ara.Location = new System.Drawing.Point(636, 394);
+            this.Btn_Ara.Name = "Btn_Ara";
+            this.Btn_Ara.Size = new System.Drawing.Size(98, 40);
+            this.Btn_Ara.TabIndex = 10;
+            this.Btn_Ara.Text = "Ara";
+            this.Btn_Ara.UseVisualStyleBackColor = true;
+            this.Btn_Ara.Click += new System.EventHandler(this.Btn_Ara_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(782, 458);
+            this.Controls.Add(this.Btn_Ara);
+            this.Controls.Add(this.Btn_Güncelle);
+            this.Controls.Add(this.Btn_Sil);
+            this.Controls.Add(this.Btn_Kaydet);
+            this.Controls.Add(this.Btn_Listele);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Kategoriler";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TxtKategoriAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_Listele;
+        private System.Windows.Forms.Button Btn_Kaydet;
+        private System.Windows.Forms.Button Btn_Sil;
+        private System.Windows.Forms.Button Btn_Güncelle;
+        private System.Windows.Forms.Button Btn_Ara;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ürün Takip/Urun_Takip/Urun_Takip/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace Urun_Takip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        //Data Source=DESKTOP-QE6JDF1\SQLEXPRESS01;Initial Catalog=DbUrun;Integrated Security=True
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-QE6JDF1\SQLEXPRESS01;Initial Catalog=DbUrun;Integrated Security=True");
+
+
+        private void Btn7_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Btn_Listele_Click(object sender, EventArgs e)
+        {
+           
+            SqlCommand cmd = new SqlCommand("Select * from TblKategori", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+            dataGridView1.DataSource=dt;
+        }
+
+        private void Btn_Kaydet_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd2 = new SqlCommand("Insert into TblKategori(Ad) Values (@p1)", conn);
+            cmd2.Parameters.AddWithValue("@p1", TxtKategoriAd.Text);
+            cmd2.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Kategoriniz başarılı bir şekilde eklendi.");
+        }
+
+        private void Btn_Sil_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd3 = new SqlCommand("Delete from TblKategori where ID=@p1", conn);
+            cmd3.Parameters.AddWithValue("@p1", textID.Text);
+            cmd3.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Kategori silme işlemi başarılı bir şekilde gerçekleşti");
+        }
+
+        private void Btn_Güncelle_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd4 = new SqlCommand("Update TblKategori set Ad=@p1 where ID=@p2", conn);
+            cmd4.Parameters.AddWithValue("@p1", TxtKategoriAd.Text);
+            cmd4.Parameters.AddWithValue("@p2", textID.Text);
+            cmd4.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Kategori güncelleme işlemi başarılı bir şekilde gerçekleşti");
+        }
+
+        private void Btn_Ara_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd5 = new SqlCommand("Select * from TblKategori where Ad=@p1", conn);
+            cmd5.Parameters.AddWithValue("@p1", TxtKategoriAd.Text);
+            SqlDataAdapter da = new SqlDataAdapter(cmd5);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+    
+}
